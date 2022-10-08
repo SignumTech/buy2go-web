92e4e5a3c8470d5d84b8cb9d88df003a0d79b980
Fnote Mehari <fnote.md@gmail.com>
2022-10-08 15:56:56 +0300

    add register user

--- app/Http/Controllers/registerUsersController.php
+++ app/Http/Controllers/registerUsersController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class registerUsersController extends Controller
 {
-    //
+    public function registerUser(Request $request){
+        $this->validate($request, [
+            'f_name' => ['required', 'string', 'max:255'],
+            'l_name' => ['required', 'string', 'max:255'],
+            //'preference' => ['required', 'string', 'max:255'],
+            'phone_no' => 'required|regex:/(01)[0-9]{9}/',
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $user = new User;
+        $user->f_name = $request->f_name;
+        $user->l_name = $request->l_name;
+        $user->phone_no = $request->phone_no;
+        $user->password = Hash::make($request->password);
+        $user->account_type = "USER";
+        $user->user_role = "USER";
+        //$user->otp = rand(1000 , 9999);
+        $user->save();
+
+        $user_token = $user->createToken($user->f_name);
+
+        return ['token' => $user_token->plainTextToken];
+    }
 }
