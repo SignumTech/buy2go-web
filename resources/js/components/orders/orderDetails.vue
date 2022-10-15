4c97e85d43dd0bda82b4edc9abcdeef73dd56a93
Fnote Mehari <fnote.md@gmail.com>
2022-10-15 19:56:01 +0300

    dot fix

--- resources/js/components/orders/orderDetails.vue
+++ resources/js/components/orders/orderDetails.vue
@@ -96,10 +96,7 @@ export default {
         }
     },
     created(){
-        window.Echo.private(`order_rejected.${this.order.id}`)
-            .listen('DriverRejectedOrder', (e) => {
-                console.log(e.order);
-            });
+
     },
     mounted(){
         this.getOrder()
@@ -127,6 +124,11 @@ export default {
             .then( response =>{
                 this.order = response.data.order_details
                 this.orderItems = response.data.order_items
+
+                window.Echo.private(`order_rejected.${this.order.id}`)
+                .listen('DriverRejectedOrder', (e) => {
+                    console.log(e.order);
+                });
                 this.getAddress(response.data.order_details.delivery_details)
             })
         },
