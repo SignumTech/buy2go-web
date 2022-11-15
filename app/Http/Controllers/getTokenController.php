64b1b0b9f97e67277b8530bea7c87a5beb6ae193
Fnote Mehari <fnote.md@gmail.com>
2022-11-15 12:18:11 +0300

    add warehouse manager login

--- app/Http/Controllers/getTokenController.php
+++ app/Http/Controllers/getTokenController.php
@@ -82,4 +82,29 @@ class getTokenController extends Controller
             return response("Wrong credentials", 401);
         }
     }
+
+    public function getWarehouseToken(Request $request){
+        $this->validate( $request, [
+            "phone_no" => "required",
+            "password" => "required|string"
+        ]);
+        $user = User::where('phone_no', $request->phone_no)->first();
+        if(!$user){
+            return response("Wrong credentials", 401);
+        }
+        if(!in_array($user->user_role, ["WAREHOUSE MANAGER"])){
+            return response("Access Denied", 401);
+        }
+        if (Hash::check($request->password, $user->password)) {
+            if (Auth::check()){
+                $request->user()->currentAccessToken()->delete();
+            }
+            $user_token = $user->createToken($user->f_name);
+
+            return ['token' => $user_token->plainTextToken]; 
+        }
+        else{
+            return response("Wrong credentials", 401);
+        }
+    }
 }
