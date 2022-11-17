455560ea099fed87008a57f5af80c97eb858cbe3
Fnote Mehari <fnote.md@gmail.com>
2022-11-17 11:46:34 +0300

    minor fixes

--- app/Events/DriverAssigned.php
+++ app/Events/DriverAssigned.php
@@ -9,7 +9,7 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-
+use App\Models\User;
 class DriverAssigned implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
