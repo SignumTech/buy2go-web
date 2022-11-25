e30bba76d4cc94237ca18809c4dbc1ad27231752
Fnote Mehari <fnote.md@gmail.com>
2022-11-25 17:39:13 +0300

    test

--- app/Http/Controllers/ordersController.php
+++ app/Http/Controllers/ordersController.php
@@ -194,6 +194,7 @@ class ordersController extends Controller
             array_push($data, $row);
         }
         var_dump($data);
+        dd($data);
         dd(array_search(min($data), $data));
         dd(array_search(min($data), $data));
         
