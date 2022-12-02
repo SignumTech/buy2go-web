56e455aec2a853be46a1cf01ebee272e227e0f5c
Fnote Mehari <fnote.md@gmail.com>
2022-12-02 17:05:57 +0300

    test

--- app/Http/Controllers/categoriesController.php
+++ app/Http/Controllers/categoriesController.php
@@ -144,12 +144,14 @@ class categoriesController extends Controller
         foreach($categories as $cat){
             $count = Product::where('cat_id', $cat->id)->count();
             $cat->items = $count + $this->categoryItemCount($cat->id);
-            if(Category::where('id', $cat->parent_id)->select('cat_name')->first()){
+            $cat->parent_name = Category::find($cat->parent_id)->cat_name;
+            /*if(Category::where('id', $cat->parent_id)->select('cat_name')->first()){
+                
                 $cat->parent_name = Category::where('id', $cat->parent_id)->select('cat_name')->first()->cat_name;
             }
             else{
                 dd($cat);
-            }
+            }*/
             
         }
         return $categories;
