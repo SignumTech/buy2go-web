4d011f6bdde00427d91eb65c005c32c8d7e66a08
Fnote Mehari <fnote.md@gmail.com>
2022-12-13 12:04:14 +0300

    test tracking

--- resources/js/components/fleet_management/trackVisitModal.vue
+++ resources/js/components/fleet_management/trackVisitModal.vue
@@ -5,11 +5,11 @@
     </div>
     <div class="col-md-12 mt-3">
         <GmapMap :center="center" :zoom="12" style="width: 100%; height: 500px" ref="mapRef">
-            <gmap-info-window :options="infoOptions" :position="address" :opened="true" @closeclick="infoWinOpen=false">
+            <gmap-info-window :options="driverMarker.infoOptions" :position="driverMarker.position" :opened="true" @closeclick="infoWinOpen=false">
             </gmap-info-window>
             <gmap-info-window v-for="m,index in markers" :key="index" :options="m.infoOptions" :position="m.position" :opened="true" @closeclick="infoWinOpen=false">
             </gmap-info-window>
-            <google-marker v-if="!loading" :icon="`/storage/settings/store.png`" :position="address" :clickable="true" :draggable="false" ></google-marker>
+            <google-marker :icon="`/storage/settings/store.png`" :position="driverMarker.position" :clickable="true" :draggable="false" ></google-marker>
             <google-marker v-for="m,index in markers" :icon="`/storage/settings/store.png`" :shape="shape" :key="index" :position="m.position" :clickable="true" :draggable="false" @click="toggleInfoWindow(m,i)"></google-marker>
             <!--<gmap-polygon v-for="path,index in paths" :key="index" :paths="path" :editable="false" :draggable="true" @paths_changed="updateEdited($event)"></gmap-polygon>-->
         </GmapMap>
@@ -22,12 +22,29 @@ export default {
     data(){
         return{
             loading:false,
+            shape: {
+                coords: [10, 10, 10, 15, 15, 15, 15, 10],
+                type: 'poly'
+            },
             center: {
                 lat: 8.9806,
                 lng: 38.7578
             },
             address:null,
             markers: [],
+            driverMarker:{
+                infoOptions:{
+                    content:"<strong>"+this.visit.driver+"</strong>",
+                    pixelOffset:{
+                        height: -35,
+                        width: 0
+                    }
+                },
+                position:{
+                    lat:9.012982,
+                    lng:38.9485312
+                }
+            },
             infoOptions: {
                 content: '',
                 //optional: offset infowindow so it visually sits nicely on top of our marker
@@ -40,7 +57,7 @@ export default {
     },
     mounted(){
         this.populateAddresses()
-
+        
     },
     methods:{
         populateAddresses(){
@@ -62,7 +79,14 @@ export default {
                     }
                 })
                 this.loading = true
+                this.startSimulation()
             });
+        },
+        startSimulation(){
+            window.setInterval(()=>{
+                this.driverMarker.position.lng -= 0.0001 
+                this.driverMarker.position.lng -= 0.0002 
+            }, 500);
         }
     }
 }
