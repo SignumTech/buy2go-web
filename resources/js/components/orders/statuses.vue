eb959871f54e8fc6635e05c7d00a9bbb94743e38
Fnote Mehari <fnote.md@gmail.com>
2022-12-19 12:03:08 +0300

    minor changes

--- resources/js/components/orders/statuses.vue
+++ resources/js/components/orders/statuses.vue
@@ -1,27 +1,27 @@
 <template>
     <div>
         <span v-if="status == `PROCESSING`" class=" px-3 badge rounded-pill bg-processing text-dark me-3">
-            <span class="fa fa-circle fs-7"></span>
+            <span class="fa fa-cart-plus fs-7"></span>
             Processing
         </span>
         <span v-if="status == `PENDING_CONFIRMATION`" class=" px-3 badge rounded-pill bg-pending_confirmation text-dark me-3">
-            <span class="fa fa-circle fs-7"></span>
+            <span class="fa fa-user-plus fs-7"></span>
             Pending Confirmation
         </span>
         <span v-if="status == `PENDING_PICKUP`" class=" px-3 badge rounded-pill bg-pending_pickup text-dark me-3">
-            <span class="fa fa-circle fs-7"></span>
+            <span class="fa fa-truck-loading fs-7"></span>
             Pending Pickup
         </span>
         <span v-if="status == `SHIPPED`" class=" px-3 badge rounded-pill bg-shipped text-dark me-3">
-            <span class="fa fa-circle fs-7"></span>
+            <span class="fa fa-shipping-fast fs-7"></span>
             Shipped
         </span>
         <span v-if="status == `DELIVERED`" class=" px-3 badge rounded-pill bg-delivered text-dark me-3">
-            <span class="fa fa-circle fs-7"></span>
+            <span class="fa fa-box-open fs-7"></span>
             Delivered
         </span>
         <span v-if="status == `CANCELED`" class=" px-3  badge rounded-pill bg-canceled text-dark me-3">
-            <span class="fa fa-circle fs-7"></span>
+            <span class="fa fa-times-circle fs-7"></span>
             Canceled
         </span>
                         
