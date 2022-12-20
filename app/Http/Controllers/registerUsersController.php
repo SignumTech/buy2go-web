cc8128ed2fd0b2d9397ed8a9cf4cea06ff43bc73
Fnote Mehari <fnote.md@gmail.com>
2022-12-20 12:07:40 +0300

    minor changes

--- app/Http/Controllers/registerUsersController.php
+++ app/Http/Controllers/registerUsersController.php
@@ -34,9 +34,9 @@ class registerUsersController extends Controller
         //$user->otp = rand(1000 , 9999);
         $user->save();
 
-        $admin = User::where('user_role', "ADMIN")->get();
+        /*$admin = User::where('user_role', "ADMIN")->get();
         $user_message = "A new customer has registered on buy2go";
-        Notification::send($admin, new ClientRegistered($user_message,$user));
+        Notification::send($admin, new ClientRegistered($user_message,$user));*/
         $user_token = $user->createToken($user->f_name);
 
         return [
